1412af2a925e8ace808cc7071486d8116d248774
przemek <przemyslaw.soltan@gmail.com>
2018-08-23 17:20:48 +0200

    update vue cli

--- src/router.js
+++ src/router.js
@@ -8,44 +8,55 @@ import Organisms from "./views/Organisms.vue";
 import Templates from "./views/Templates.vue";
 import Pages from "./views/Pages.vue";
 
+
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: "/",
-      name: "main",
-      component: Main
-    },
-    {
-      path: "/atoms",
-      name: "atoms",
-      component: Atoms
-    },
-    {
-      path: "/molecules",
-      name: "molecules",
-      component: Molecules
-    },
-    {
-      path: "/grid",
-      name: "grid",
-      component: Grid
-    },
-    {
-      path: "/organisms",
-      name: "organisms",
-      component: Organisms
-    },
-    {
-      path: "/templates",
-      name: "templates",
-      component: Templates
-    },
-    {
-      path: "/pages",
-      name: "pages",
-      component: Pages
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+          path: "/",
+          name: "main",
+          component: Main
+        },
+        {
+          path: "/atoms",
+          name: "atoms",
+          component: Atoms
+        },
+        {
+          path: "/molecules",
+          name: "molecules",
+          component: Molecules
+        },
+        {
+          path: "/grid",
+          name: "grid",
+          component: Grid
+        },
+        {
+          path: "/organisms",
+          name: "organisms",
+          component: Organisms
+        },
+        {
+          path: "/templates",
+          name: "templates",
+          component: Templates
+        },
+        {
+          path: "/pages",
+          name: "pages",
+          component: Pages
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        }
+    ]
 });
